263d64d3d450014b52dbfe0ceafb4fc35ad40a58
thts <tschachtli.th@gmail.com>
2018-11-18 16:02:24 +0100

    tour of heroes

--- tour-of-heroes/angular-tour-of-heroes3-http/src/app/app.module.ts
+++ tour-of-heroes/angular-tour-of-heroes3-http/src/app/app.module.ts
@@ -1,15 +1,24 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
-import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {HeroesComponent} from './heroes/heroes.component';
+import {FormsModule} from '@angular/forms';
+import {HeroDetailComponent} from './hero-detail/hero-detail.component';
+import {MessagesComponent} from './messages/messages.component';
+import {AppRoutingModule} from './app-routing.module';
+import {DashboardComponent} from './dashboard/dashboard.component'; // NgModel lives here
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
